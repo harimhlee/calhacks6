5559cb92d5a3bd74a9fa28803de186d061ea97d7
harimhlee <harimlee@berkeley.edu>
2019-10-26 12:22:47 -0700

    test

---
+++ face_detect/face_detect/BlinkDetector.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  face_detect
+//
+//  Created by Harim Lee on 10/26/19.
+//  Copyright © 2019 Harim Lee. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class BlinkDetector: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
